d8b92b2d26c396af27a647dfdebf0f8b7348e8e8
Davy Landman <davy.landman@gmail.com>
2020-01-22 13:12:06 +0100

    Made the algomized header a bit nicer

--- algamize.sh
+++ algamize.sh
@@ -2,7 +2,7 @@
 SRC_DIR="src/"
 
 NESTED_FILES=("c25519/f25519" "c25519/fprime" "c25519/sha512" "c25519/c25519" "c25519/ed25519" "c25519/edsign" )
-COMPACT_FILES=(compact_x25519 compact_ed25519 erase_from_memory)
+COMPACT_FILES=(compact_x25519 compact_ed25519 compact_wipe)
 
 DST_DIR="$1"
 mkdir -p "$DST_DIR"
@@ -27,19 +27,38 @@ function remove_local_imports() {
     sed 's/#include ".*h"//'
 }
 
+function merge_includes() {
+    awk '
+    /#include .*/ { includes[$0] = 1; next;}
+    { other[NR] = $0; next; }
+    END {
+        for (i in includes) {
+            print i;
+        }
+        for (i in other) {
+            print other[i];
+        }
+    }
+    ' | cat -s # remove double blank lines
+}
+
 echo "// compact25519 $VERSION
+// Licensed under CC0-1.0
+// Based on Daniel Beer's Public Domain C25519 implementation
 #ifndef __COMPACT_25519_H
 #define __COMPACT_25519_H
 " > "$DST_HEADER"
 
 for h in "${COMPACT_FILES[@]}"; do 
-    cat "$SRC_DIR/$h.h" | remove_header_guard >> "$DST_HEADER"
-done
+    cat "$SRC_DIR/$h.h" | remove_header_guard 
+done | merge_includes >> "$DST_HEADER" 
 
 echo "#endif" >> "$DST_HEADER"
 
 
 echo "// compact25519 $VERSION
+// Licensed under CC0-1.0
+// Based on Daniel Beer's Public Domain C25519 implementation
 #include \"compact25519.h\"
 " > "$DST_SOURCE"
 
