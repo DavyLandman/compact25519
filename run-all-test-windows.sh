787e81c9f8b5b256c3411c6974f137aebffa2917
Davy Landman <davy.landman@gmail.com>
2020-01-27 21:19:29 +0100

    Renaming script to run test on windows without c compiler available

--- run-all-test-windows.sh
+++ run-all-test-windows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run --rm -v "/$(PWD):/app" silkeh/clang bash -c 'cd /app && CC=clang CFLAGS="-fsanitize=undefined" make clean test'
-docker run --rm -v "/$(PWD):/app" golang:1.13 bash -c 'cd /app && make test-dist'
+mingw32-make test-dist
