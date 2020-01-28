6e8438115ef35490bdcce89e00eab8cdff25c8b4
Davy Landman <davy.landman@gmail.com>
2020-01-28 09:21:20 +0100

    Fixed formatting of test file

--- test/algamize-test.go
+++ test/algamize-test.go
@@ -1,17 +1,17 @@
-package main 
+package main
 
 // #include "../dist/compact25519.h"
 // #include "../dist/compact25519.c"
 import "C"
 
 import (
-	"unsafe"
-	"fmt"
-	"log"
-	"crypto/rand"
 	"bytes"
 	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
 	"golang.org/x/crypto/curve25519"
+	"log"
+	"unsafe"
 )
 
 func bytePointer(ar []byte) *C.uint8_t {
@@ -23,7 +23,6 @@ func x25519FullCompact() {
 	var seed1, seed2 [C.X25519_KEY_SIZE]byte
 	rand.Read(seed1[:])
 	rand.Read(seed2[:])
-
 
 	var sec1, pub1, sec2, pub2 [C.X25519_KEY_SIZE]byte
 	C.compact_x25519_keygen(bytePointer(sec1[:]), bytePointer(pub1[:]), bytePointer(seed1[:]))
@@ -81,50 +80,50 @@ func x25519Interop() {
 }
 
 func ed25519Compact() {
-	fmt.Print("Testing ed25519 compact: ");
+	fmt.Print("Testing ed25519 compact: ")
 	var seed [C.ED25519_SEED_SIZE]byte
 	rand.Read(seed[:])
 
 	var sec [C.ED25519_PRIVATE_KEY_SIZE]byte
 	var pub [C.ED25519_PUBLIC_KEY_SIZE]byte
 
-    C.compact_ed25519_keygen(bytePointer(sec[:]), bytePointer(pub[:]), bytePointer(seed[:]));
+	C.compact_ed25519_keygen(bytePointer(sec[:]), bytePointer(pub[:]), bytePointer(seed[:]))
 
 	var msg [200]byte
 	rand.Read(msg[:])
 
 	var sig [C.ED25519_SIGNATURE_SIZE]byte
-    C.compact_ed25519_sign(bytePointer(sig[:]), bytePointer(sec[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg)));
+	C.compact_ed25519_sign(bytePointer(sig[:]), bytePointer(sec[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg)))
 
-    if !C.compact_ed25519_verify(bytePointer(sig[:]), bytePointer(pub[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
-        fmt.Print("** Failed normal signature\n");
-        return;
-    }
+	if !C.compact_ed25519_verify(bytePointer(sig[:]), bytePointer(pub[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
+		fmt.Print("** Failed normal signature\n")
+		return
+	}
 
-    msg[3] ^= 0x10; // flip a single bit
-    if C.compact_ed25519_verify(bytePointer(sig[:]), bytePointer(pub[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
-        fmt.Print("** Failed changed data\n");
-    } else {
-		fmt.Println("OK");
-    }
+	msg[3] ^= 0x10 // flip a single bit
+	if C.compact_ed25519_verify(bytePointer(sig[:]), bytePointer(pub[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
+		fmt.Print("** Failed changed data\n")
+	} else {
+		fmt.Println("OK")
+	}
 }
 
 func ed25519Interop() {
 
-	fmt.Print("Testing ed25519 interop: ");
+	fmt.Print("Testing ed25519 interop: ")
 	var seed [C.ED25519_SEED_SIZE]byte
 	rand.Read(seed[:])
 
 	var sec [C.ED25519_PRIVATE_KEY_SIZE]byte
 	var pub [C.ED25519_PUBLIC_KEY_SIZE]byte
 
-    C.compact_ed25519_keygen(bytePointer(sec[:]), bytePointer(pub[:]), bytePointer(seed[:]));
+	C.compact_ed25519_keygen(bytePointer(sec[:]), bytePointer(pub[:]), bytePointer(seed[:]))
 
 	var msg [200]byte
 	rand.Read(msg[:])
 
 	var sig [C.ED25519_SIGNATURE_SIZE]byte
-	C.compact_ed25519_sign(bytePointer(sig[:]), bytePointer(sec[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg)));
+	C.compact_ed25519_sign(bytePointer(sig[:]), bytePointer(sec[:]), unsafe.Pointer(&msg[0]), C.size_t(len(msg)))
 
 	if !ed25519.Verify(ed25519.PublicKey(pub[:]), msg[:], sig[:]) {
 		fmt.Println("** Failure golang verify")
@@ -137,16 +136,14 @@ func ed25519Interop() {
 	}
 	goSignature := ed25519.Sign(goPrivate, msg[:])
 
-
-    if !C.compact_ed25519_verify(bytePointer(goSignature), bytePointer(goPublic), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
+	if !C.compact_ed25519_verify(bytePointer(goSignature), bytePointer(goPublic), unsafe.Pointer(&msg[0]), C.size_t(len(msg))) {
 		fmt.Println("** Failure to verify signature from golang")
 	}
-
 
 	var sig2 [C.ED25519_SIGNATURE_SIZE]byte
 	C.compact_ed25519_sign(bytePointer(sig2[:]), bytePointer(goPrivate), unsafe.Pointer(&msg[0]), C.size_t(len(msg)))
 	if bytes.Equal(goSignature, sig2[:]) {
-		fmt.Println("OK");
+		fmt.Println("OK")
 	} else {
 		fmt.Println("** Failure to use go generated keypair")
 	}
