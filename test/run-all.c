abdb071c991dd5339efb43d9a233f4c494ddca56
Davy Landman <davy.landman@gmail.com>
2020-01-28 20:39:38 +0100

    Make tests actually return non zero in case of failure

--- test/run-all.c
+++ test/run-all.c
@@ -28,7 +28,7 @@ static void fill_crappy_random(void* target, size_t length, pcg32_random_t* rng)
 }
 
 #ifndef COMPACT_DISABLE_X25519
-static void testX25519(pcg32_random_t* rng) {
+static int testX25519(pcg32_random_t* rng) {
     printf("Testing X25519: ");
     uint8_t seed1[X25519_KEY_SIZE];
     uint8_t seed2[X25519_KEY_SIZE];
@@ -52,13 +52,20 @@ static void testX25519(pcg32_random_t* rng) {
     compact_x25519_derive_encryption_key(derived, sizeof(derived), shared1, pub1, pub2);
 #endif
 
-    printf(memcmp(shared1, shared2, X25519_SHARED_SIZE) == 0 ? "Success\n" : "Fail\n");
+    if (memcmp(shared1, shared2, X25519_SHARED_SIZE) == 0) {
+        printf("Success\n");
+        return 0;
+    }
+    else {
+        printf("Fail\n");
+        return -1;
+    }
 }
 #endif
 
 #ifndef COMPACT_DISABLE_ED25519
 #define MSG_TEST_SIZE (ED25519_PRIVATE_KEY_SIZE * 2)
-static void testEd25519(pcg32_random_t* rng) {
+static int testEd25519(pcg32_random_t* rng) {
     printf("Testing Ed25519: ");
     uint8_t seed[ED25519_SEED_SIZE];
     fill_crappy_random(seed, ED25519_SEED_SIZE, rng);
@@ -75,16 +82,18 @@ static void testEd25519(pcg32_random_t* rng) {
 
     if (!compact_ed25519_verify(sig, pub, msg, MSG_TEST_SIZE)) {
         printf("Failed normal signature\n");
-        return;
+        return -1;
     }
 
     msg[3] ^= 0x10; // flip a single bit
     if (compact_ed25519_verify(sig, pub, msg, MSG_TEST_SIZE)) {
         printf("Failed on changed data\n");
+        return -1;
     }
     else {
         printf("Success\n");
     }
+    return 0;
 }
 #endif
 
@@ -94,15 +103,17 @@ int main(void) {
     rng.state = rand();
     rng.inc = rand() | 1;
 
+    int result = 0;
 #ifndef COMPACT_DISABLE_X25519
     for (int i = 0; i < 5; i++) {
-        testX25519(&rng);
+        result += testX25519(&rng);
     }
 #endif
 
 #ifndef COMPACT_DISABLE_ED25519
     for (int i = 0; i < 5; i++) {
-        testEd25519(&rng);
+        result += testEd25519(&rng);
     }
 #endif
+    return result;
 }
